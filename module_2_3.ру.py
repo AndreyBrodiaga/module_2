68135909cda543d8725b6ddeadf62a9465c1a1b9
Andrey <andrei.sergatchev87@gmail.com>
2024-11-09 12:44:27 +0300

    Add files via upload

--- module_2_3.ру.py
+++ module_2_3.ру.py
@@ -1,9 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 my_list_1 = []
+i = 0
 
-for number in my_list:
-    if number <= 0:
+while i < len(my_list):
+    if my_list[i] >= 0:
+        my_list_1.append(my_list[i])
+    else:
         break
-    my_list_1.append(number)
+
+    i += 1
 
 print(my_list_1)
