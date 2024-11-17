a6b0e5f24b78daf4a847451500511ebaeb2caafe
Andrey <andrei.sergatchev87@gmail.com>
2024-11-17 15:35:29 +0300

    Add files via upload

---
+++ main.ру.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98)
+print(1000 == 100.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+(2 * 2 + 2) == (2 * (2 + 2))
+
+# 4th program
+str_point = '123.456'
+str_point_1 = float(str_point)
+integer_part = int(str_point_1)
+frctional_part = (str_point_1 - integer_part) * 10
+after_comma = int(frctional_part)
+print(after_comma)
