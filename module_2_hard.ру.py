e5699902d3e4bcf67b2b4a6e7223beffa14a75fd
Andrey <andrei.sergatchev87@gmail.com>
2024-11-18 20:51:43 +0300

    Update module_2_hard.ру.py

--- module_2_hard.ру.py
+++ module_2_hard.ру.py
@@ -1,19 +1,26 @@
 def find_pairs(target_sum, range_limit):
-    pairs = set() 
+    pairs = set()
 
     for a in range(1, range_limit + 1):
         for b in range(1, range_limit + 1):
             total = a + b
-               if total == target_sum:
-                    pairs.add((min(a, b), max(a, b)))  
-    return pairs  
+            
+            if total == target_sum:
+                
+                pairs.add((min(a, b), max(a, b)))
+
+    return pairs
+
+
 
 target_sum = int(input('введите число от 3 до 20: '))
 range_limit = 20
 
+
 result_pairs = find_pairs(target_sum, range_limit)
+
 
 result_string = ''.join(f"{a}{b}" for a, b in result_pairs)
 
-print(f"ваш код :")
+
 print(result_string)
