72836df755dec12864c163b8ee2a208ed0649788
Andrey <andrei.sergatchev87@gmail.com>
2024-11-25 06:25:58 +0300

    Update module_2_hard.ру.py

--- module_2_hard.ру.py
+++ module_2_hard.ру.py
@@ -1,27 +1,19 @@
-def find_pairs(target_sum, range_limit):
-    pairs = set()
+def generate_password(n):
+    result = ''
+    a = 1
+    while a <= 20:
+        b = a + 1
+        while b <= 20:
+            if n % (a + b) == 0:
+                result += str(a) + str(b)
+            b += 1
+        a += 1
 
-    for a in range(1, range_limit + 1):
-        for b in range(1, range_limit + 1):
-            total = a + b
-            
-            if total == target_sum:
-                
-                pairs.add((min(a, b), max(a, b)))
+    return f'{n}-{result}'
 
-    return pairs
+n = 3
+while n <= 20:
+    print(generate_password(n))
+    n += 1
 
-
-
-target_sum = int(input('введите число от 3 до 20: '))
-range_limit = 20
-
-
-result_pairs = find_pairs(target_sum, range_limit)
-
-
-result_string = ''.join(f"{a}{b}" for a, b in result_pairs)
-
-
-print(result_string)
 
